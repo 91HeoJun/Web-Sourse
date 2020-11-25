abe46c8720c8babdc3e64bf09f9c02f341c85501
[Jun Heo] <[c_atuniel@naver.com]>
2020-11-25 18:26:12 +0900

    resave

--- basic/src/basic/FourServlet.java
+++ basic/src/basic/FourServlet.java
@@ -17,16 +17,18 @@ public class FourServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 사직연산 결과
 		int num1 = Integer.parseInt(request.getParameter("num1"));
 		int num2 = Integer.parseInt(request.getParameter("num2"));
 		String op = request.getParameter("op");
-		
+
 		int result = 0;
-		
+
 		switch (op) {
 
 		case "+":
@@ -45,25 +47,27 @@ public class FourServlet extends HttpServlet {
 			result = num1 / num2;
 
 			break;
-						
+
 		default:
 			break;
 		}
-		
+
 		response.setContentType("text/html;charset=utf-8");
-		
+
 		PrintWriter out = response.getWriter();
 		out.print("<html><head><title> 사칙연산 프로그램 </title></head>");
 		out.print("<body><h2> 연산 결과 </h2><h3>");
 		out.printf("%d %s %d = %d", num1, op, num2, result);
 		out.print("</h3></body></html>");
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
